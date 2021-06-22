793bf50d3d71eaf2679834f57a6e0542b2a2d2b3
Xinbang-Hu <1822903064@qq.com>
2021-06-22 15:43:54 +0800

    add publickey page & textarea to be done

--- src/views/publickey.vue
+++ src/views/publickey.vue
@@ -15,11 +15,40 @@
         <el-menu-item index="2-2" @click="logOut">登出</el-menu-item>
       </el-submenu>
     </el-menu>
+    <el-button
+      type="primary"
+      style="margin-top: 3em; margin-left: 50em; background: #409EFF;"
+      @click="dalogSubmitKeyVisible = true">{{ isSetKey == false? '上传密钥文件':'修改密钥文件' }}
+    </el-button>
+    <el-dialog title="上传密钥" :visible.sync="dalogSubmitKeyVisible">
+    <el-upload
+      drag
+      action="https://jsonplaceholder.typicode.com/posts/"
+      style="text-align: center;"
+      multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将密钥文件拖到此处，或<em>点击上传</em></div>
+    </el-upload>
+    <div slot="footer">
+      <el-button type="primary" style="background: #409EFF" @click="dalogSubmitKeyVisible = false">确 定</el-button>
+      <el-button @click="dalogSubmitKeyVisible = false">取 消</el-button>
+    </div>
+    </el-dialog>
+    <!-- TODO: publickey textarea & private textarea -->
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      isSetKey: false,
+      dalogSubmitKeyVisible: false
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
